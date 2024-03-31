05c8b3dedad971652d32f6d394ab52e90b3ad8ca
eayildiz <emiralpery@gmail.com>
2024-03-31 21:22:58 +0300

    Util.java appropiate tests are added. %100 coverage has been reached.

--- my-app/src/test/java/TestUtil.java
+++ my-app/src/test/java/TestUtil.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -13,4 +14,40 @@ public class TestUtil {
 
   @Test
   public void example() { assertTrue(true); }
+
+  @Test
+  public void oneArg()
+  {
+    assertFalse(c.compute(8));
+  }
+
+  @Test
+  public void evenArgs()
+  {
+    assertFalse(c.compute(5, 48, 94, 1));
+  }
+
+  @Test
+  public void oddFalseCall()
+  {
+    assertFalse(c.compute(50, 45, 78));
+  }
+
+  @Test
+  public void trueCall1()
+  {
+    assertTrue(c.compute(2, 3, 1));
+  }
+  
+  @Test
+  public void trueCall2()
+  {
+    assertTrue(c.compute(23, 10, 7));
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void zeroRuntimeExc()
+  {
+    c.compute(2, 4, 0);
+  }
 }
